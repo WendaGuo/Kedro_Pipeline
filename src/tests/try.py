dcb808b8baf13c0a4d3f30d227d2eaf20ee50413
Wenda Guo <wg950914@gmail.com>
2020-04-28 19:06:58 -0400

    try a test

--- src/tests/try.py
+++ src/tests/try.py
@@ -1,6 +1,8 @@
+"""
+This is a simple experiment to check version control functions.
+"""
+
 import os
 
 if __name__ == "__main__":
-    cwd = os.getcwd()
-    ind=cwd.find("src")
-    print(cwd[:ind])
+    print("good try!")
